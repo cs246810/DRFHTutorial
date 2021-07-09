bb7b757cd7bc046b881bfb4f7395323263be9f9f
congshi <congshi.hello@gmail.com>
2021-07-09 21:33:49 +0800

    fix:user

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Note(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     pub_date = models.DateTimeField()
     title = models.CharField(max_length=200)
     body = models.TextField()
